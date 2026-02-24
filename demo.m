6d2adbe64bf217bb59368743804da1a0f7929630
Samson Weiser <samson.weiser@gmail.com>
2026-02-24 10:20:50 -0500

    initial demo

---
+++ demo.m
@@ -0,0 +1,17 @@
+function score = demo(n)
+%DEMO Tiny MATLAB demo for teaching git
+% score = demo(n) runs n steps and returns a score.
+
+if nargin < 1
+    n = 5;
+end
+
+state = 0;
+for i = 1:n
+    state = state + i;
+end
+
+score = state;
+
+fprintf("demo: n=%d state=%d score=%d\n", n, state, score);
+end
